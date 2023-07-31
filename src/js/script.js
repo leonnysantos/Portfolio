f9a16bb557484e8807e7c3c593bcb563a1befccd
Leonny Santos <130412073+leonnysantos@users.noreply.github.com>
2023-07-30 23:32:02 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,34 @@
+const voltarTopo = document.getElementById('voltarTopo');
+const socialMedia = document.getElementById('socialMedia')
+
+const scrollButton = 500;
+const scrollMedia = 3000;
+
+const verificarScrollButton = () => {
+    if (window.pageYOffset > scrollButton) {
+        voltarTopo.style.display = 'block';
+    } else {
+        voltarTopo.style.display = 'none';
+    }
+}
+
+const voltarAoTopo = () => {
+    if (window.pageYOffset > 0) {
+        window.scrollBy(0, -50);
+        setTimeout(voltarAoTopo, 5);
+    }
+}
+
+const verificarScrollMedia = () => {
+    if(window.pageYOffset > scrollMedia){
+        socialMedia.style.display = 'none';
+    } else {
+        socialMedia.style.display = 'flex';
+    }
+}
+
+window.addEventListener('scroll', verificarScrollButton);
+window.addEventListener('scroll', verificarScrollMedia);
+voltarTopo.addEventListener('click', voltarAoTopo);
+
+
